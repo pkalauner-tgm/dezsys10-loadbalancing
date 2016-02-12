4547688689593fe8c5f75119b231e65095e9d793
Paul Kalauner <pkalauner@student.tgm.ac.at>
2016-02-12 12:03:50 +0100

    added client module

---
+++ client/src/main/java/at/kalaunermalik/dezsys10/client/Client.java
@@ -0,0 +1,23 @@
+package at.kalaunermalik.dezsys10.client;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Client
+ *
+ * @author Paul Kalauner 5BHIT
+ * @version 20161202.1
+ */
+public class Client {
+    private static final Logger LOGGER = LogManager.getLogger(Client.class);
+
+    /**
+     * Main-Method
+     * @param args CLI-args
+     */
+    public static void main(String[] args) {
+        LOGGER.info("Starting Client...");
+    }
+}
